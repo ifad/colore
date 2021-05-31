97b5e591edd5f8412a2d308bf44cfb811377b875
Marcello Barnaba <vjt@openssl.it>
2021-06-01 00:40:36 +0200

    redis: allow full configurability

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@ require 'sidekiq'
 require 'config'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: Colore::C_.redis_url, namespace: Colore::C_.redis_namespace }
+  config.redis = Colore::C_.redis
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Colore::C_.redis_url, namespace: Colore::C_.redis_namespace }
+  config.redis = Colore::C_.redis
 end
