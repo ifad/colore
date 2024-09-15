59ca91cea15740db4aee9435fd6562e233d7c20c
Geremia Taglialatela <tagliala.dev@gmail.com>
2024-09-15 09:21:41 +0200

    Remove single line methods

--- lib/errors.rb
+++ lib/errors.rb
@@ -11,35 +11,51 @@ module Colore
   end
 
   class InvalidParameter < Error
-    def initialize(param = nil); super 400, "Invalid parameter #{param}"; end
+    def initialize(param = nil)
+      super 400, "Invalid parameter #{param}"
+    end
   end
 
   class DocumentExists < Error
-    def initialize; super 409, 'A document with this doc_id already exists'; end
+    def initialize
+      super 409, 'A document with this doc_id already exists'
+    end
   end
 
   class DocumentNotFound < Error
-    def initialize; super 404, 'Document not found'; end
+    def initialize
+      super 404, 'Document not found'
+    end
   end
 
   class VersionNotFound < Error
-    def initialize; super 400, 'Version not found'; end
+    def initialize
+      super 400, 'Version not found'
+    end
   end
 
   class InvalidVersion < Error
-    def initialize; super 400, 'Invalid version name'; end
+    def initialize
+      super 400, 'Invalid version name'
+    end
   end
 
   class VersionIsCurrent < Error
-    def initialize; super 400, 'Version is current, change current version first'; end
+    def initialize
+      super 400, 'Version is current, change current version first'
+    end
   end
 
   class InvalidAction < Error
-    def initialize(message = nil); super 400, (message || 'Invalid action'); end
+    def initialize(message = nil)
+      super 400, (message || 'Invalid action')
+    end
   end
 
   class FileNotFound < Error
-    def initialize; super 400, 'Requested file not found'; end
+    def initialize
+      super 400, 'Requested file not found'
+    end
   end
 
   class ConversionError < Error
