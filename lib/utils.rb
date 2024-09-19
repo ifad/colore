ced2e0a9d53c63e71dbb9522d20a3caa200503ae
Geremia Taglialatela <tagliala.dev@gmail.com>
2024-09-19 11:57:27 +0200

    Move language alpha3 conversion to Utils
    Also fix a minor Style/RedundantCondition offense

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'iso-639'
+
 module Colore
   module Utils
     # Deep conversion of all hash keys to symbols.
@@ -14,6 +16,14 @@ module Colore
       else
         obj
       end
+    end
+
+    # Converts an ISO 639-1 (alpha-2) language code to its corresponding ISO 639-2 (alpha-3) code.
+    #
+    # @param [String] lang_alpha2 The ISO 639-1 (alpha-2) language code.
+    # @return [String] The ISO 639-2 (alpha-3) language code, or `nil` if the alpha-2 code is not found.
+    def self.lang_alpha3(lang_alpha2)
+      ISO_639.find(lang_alpha2)&.alpha3
     end
   end
 end
